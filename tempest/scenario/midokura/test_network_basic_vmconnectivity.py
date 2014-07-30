1a30544ed13b14b2d2c66b9a16dc117f19c4bab2
Albert Vico <albert.vico@midokura.com>
2014-07-30 15:17:55 +0200

    test_network_basic_vmconnectivity fixes

--- tempest/scenario/midokura/test_network_basic_vmconnectivity.py
+++ tempest/scenario/midokura/test_network_basic_vmconnectivity.py
@@ -77,11 +77,9 @@ class TestNetworkBasicVMConnectivity(scenario.TestScenario):
         #tenant = self.tenants[self.tenant_id]
         access_point_ssh = self._connect_to_access_point(self.access_point)
         for server in self.servers:
-            if server.id != self.access_point.id:
-                dest = self._get_server_ip(server)
-                self._check_connectivity(access_point=access_point_ssh,
-                                         ip=dest)
-                access_point_ssh.ping_host(dest)
+            dest = self._get_server_ip(server)
+            self._check_connectivity(access_point_ssh,dest)
+            access_point_ssh.ping_host(dest)
 
     def _check_connectivity(self, access_point, ip, should_succeed=True):
         if should_succeed:
