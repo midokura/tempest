329053ca4293743b82163b0d56f9f1b67360791e
Albert Vico <albert.vico@midokura.com>
2014-08-05 18:41:13 +0200

    Fixes for checking route content

--- tempest/scenario/midokura/midotools/helper.py
+++ tempest/scenario/midokura/midotools/helper.py
@@ -14,29 +14,31 @@ class Routetable:
             cls.init_from_line(args[0])
             return
 
-        (cls.dest, cls.gateway, cls.iface) = args
-        if len(args) > 3 :
-            cls.mask = args[3]
+        (cls.destination, cls.gateway, cls.iface) = args
+        if len(args) > 3:
+            cls.genmask = args[3]
 
     def init_from_line(self, line):
         """
         default         10.10.1.1       0.0.0.0         U     0      0      0       eth0
         """
         cols = line.split(None)
+        try:
+            if len(cols) < 8 or not cols or cols[0] is "Destination":
+                self.destination = None
+                return
+            self.destination = cols[0]
+            self.gateway = cols[1]
+            self.genmask = cols[2]
+            self.flags = cols[3]
+            self.metric = cols[4]
+            self.ref = cols[5]
+            self.use = cols[6]
+            self.iface = cols[7]
+        except Exception as error:
+            raise error
 
-        if len(cols) < 8 or not cols or cols[0] is "Destination":
-            self.destination = None
-            return
-        self.destination = cols[0]
-        self.gateway = cols[1]
-        self.genmask = cols[2]
-        self.flags = cols[3]
-        self.metric = cols[4]
-        self.ref = cols[5]
-        self.use = cols[6]
-        self.iface = cols[7]
-
-    def __repr__(self) :
+    def __repr__(self):
         """Return a string representing the route"""
         return "dest=%-16s gw=%-16s mask=%-16s iface=%s" % (self.destination,
                                                             self.gateway,
