9308fd40c194430f7b26b7d4b6ec807b4849ad3a
Albert Vico <albert.vico@midokura.com>
2014-08-05 17:22:12 +0200

    Fixes for checking route content

--- tempest/scenario/midokura/test_network_basic_vmconnectivity.py
+++ tempest/scenario/midokura/test_network_basic_vmconnectivity.py
@@ -118,6 +118,7 @@ class TestNetworkBasicVMConnectivity(scenario.TestScenario):
         pprint(route_out)
         for line in lines[2:]:
             cols = line.split(None)
+            pprint(cols)
             route_table['destination'].append(cols[0])
             route_table['gateway'].append(cols[1])
             route_table['genmask'].append(cols[2])
@@ -126,6 +127,7 @@ class TestNetworkBasicVMConnectivity(scenario.TestScenario):
             route_table['ref'].append(cols[5])
             route_table['use'].append(cols[6])
             route_table['Iface'].append(cols[7])
+
         pprint(route_table)
         return route_table
 
