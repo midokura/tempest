1e169a8d36f489db6ead7d720e53ce84584a451d
Albert Vico <albert.vico@midokura.com>
2014-08-06 12:24:02 +0200

    Test inter_vmconnectivity

--- tempest/scenario/midokura/test_network_basic_inter_vmconnectivity.py
+++ tempest/scenario/midokura/test_network_basic_inter_vmconnectivity.py
@@ -7,49 +7,45 @@ it should be able to metadata service.
 Pre-requisites:
 1 tenant
 1 network
-1 VM
+2 VMs
 
 Steps:
 1. create a network
-2. launch a VM
-3. verify that the VM gets IP address
-4. verify that the VM gets default GW in the routing table
-5. verify that the VM gets a routing entry for metadata service via dhcp agent
+2. launch 2 VMs
+3. verify that 2 VMs can ping each other
 
 Expected results:
-vm should get an ip address (confirm by "ip addr" command) 
-VM should get a defaut gw
-VM should get a route for 169.254.169.254 (on non-cirros )
-
+ping works
 '''
 
 
-
+from tempest.test import services
 from tempest import config
 from tempest.openstack.common import log as logging
 from tempest.scenario.midokura.midotools import scenario
-from tempest.test import services
-from tempest.scenario.midokura.midotools import helper
-import re
+import itertools
+
 from pprint import pprint
 
 CONF = config.CONF
 LOG = logging.getLogger(__name__)
 CIDR1 = "10.10.1.0/24"
 
-class TestNetworkBasicVMConnectivity(scenario.TestScenario):
+class TestNetworkBasicIntraVMConnectivity(scenario.TestScenario):
 
     @classmethod
     def setUpClass(cls):
-        super(TestNetworkBasicVMConnectivity, cls).setUpClass()
+        super(TestNetworkBasicIntraVMConnectivity, cls).setUpClass()
         cls.check_preconditions()
 
     def setUp(self):
-        super(TestNetworkBasicVMConnectivity, self).setUp()
+        super(TestNetworkBasicIntraVMConnectivity, self).setUp()
         self.security_group = \
             self._create_security_group_neutron(tenant_id=self.tenant_id)
         self._scenario_conf()
         self.custom_scenario(self.scenario)
+
+
 
     def _scenario_conf(self):
         serverB = {
@@ -63,7 +59,7 @@ class TestNetworkBasicVMConnectivity(scenario.TestScenario):
         }
         networkA = {
             'subnets': [subnetA],
-            'servers': [serverB],
+            'servers': [serverB, serverB],
         }
         tenantA = {
             'networks': [networkA],
@@ -75,47 +71,32 @@ class TestNetworkBasicVMConnectivity(scenario.TestScenario):
             'tenants': [tenantA],
         }
 
-    def _serious_test(self, remote_ip, pk):
-        LOG.info("Trying to get the list of ips")
+
+    def _ping_through_gateway(self, origin, destination):
+        LOG.info("Trying to ping the list of ips")
         try:
-            ssh_client = self.setup_tunnel(remote_ip, pk)
-            net_info = ssh_client.get_ip_list()
-            LOG.debug(net_info)
-            pattern = re.compile('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-            list = pattern.findall(net_info)
-            LOG.debug(list)
-            self.assertIn(remote_ip, list)
-            route_out = ssh_client.exec_command("sudo /sbin/route -n")
-            self._check_default_gateway(route_out, remote_ip)
-            LOG.info(route_out)
+            ssh_client = self.setup_tunnel(origin[0], origin[1])
+            self.assertTrue(self._check_remote_connectivity(ssh_client,destination))
         except Exception as inst:
             LOG.info(inst.args)
             LOG.info
             raise
 
-    def _check_default_gateway(self, route_out, internal_ip):
-        try:
-            rtable = helper.Routetable.build_route_table(route_out)
-            ##TODO: More extended route table tests
-            LOG.debug(rtable)
-            self.assertTrue(any([r.is_default_route() for r in rtable]))
-        except Exception as inst:
-            LOG.info(inst.args)
-            raise
-
     @services('compute', 'network')
-    def test_network_basic_vmconnectivity(self):
+    def test_network_basic_inter_vmconnectivity(self):
         ap_details = self.access_point.keys()[0]
         networks = ap_details.networks
+        ip_pk = []
         for server in self.servers:
             #servers should only have 1 network
             name = server.networks.keys()[0]
             if any(i in networks.keys() for i in server.networks.keys()):
                 remote_ip = server.networks[name][0]
                 pk = self.servers[server].private_key
-                self._serious_test(remote_ip, pk)
+                ip_pk.append((remote_ip, pk))
             else:
                 LOG.info("FAIL - No ip connectivity to the server ip: %s" % server.networks[name][0])
                 raise Exception("FAIL - No ip for this network : %s"
                             % server.networks)
-
+        for pair in itertools.permutations(ip_pk):
+            self._ping_through_gateway(pair[0],pair[1])
